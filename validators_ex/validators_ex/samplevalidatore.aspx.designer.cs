4995b553b3dffb3cf3cb60e75bb0f2549021709a
Sudha AnandaKumar <sudhaanand20@outlook.com>
2024-04-11 11:18:04 -0500

    Redesigned webpage with styles and validators.

--- validators_ex/validators_ex/samplevalidatore.aspx.designer.cs
+++ validators_ex/validators_ex/samplevalidatore.aspx.designer.cs
@@ -40,13 +40,13 @@ namespace validators_ex {
         protected global::System.Web.UI.WebControls.TextBox txtfirstname;
         
         /// <summary>
-        /// Label1 control.
+        /// RequiredFieldValidator3 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
         
         /// <summary>
         /// RegularExpressionValidator1 control.
